a7330bb21cc388587e2dc53a0d9e9f92cebfe2fc
DarshTank <darshtank05@gmail.com>
2025-01-27 11:56:20 +0530

    Fontend Setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Correct path to your HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // Match all JS, TS, JSX, and TSX files in src/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
